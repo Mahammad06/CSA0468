2b98a23c30921d5a3b3ec7272262be48091dbab2
Mahammad06 <112552412+Mahammad06@users.noreply.github.com>
2023-11-02 12:22:16 +0530

    Add files via upload

---
+++ FCFS.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    int n, total_wait_time = 0, total_turnaround_time = 0;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    int burst_time[n], wait_time[n], turnaround_time[n];
+
+    // Input burst times for each process
+    printf("Enter Burst Time:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &burst_time[i]);
+    }
+
+    wait_time[0] = 0; // Waiting time for the first process is 0
+
+    // Calculate waiting times for the remaining processes
+    for (int i = 1; i < n; i++) {
+        wait_time[i] = wait_time[i - 1] + burst_time[i - 1];
+        total_wait_time += wait_time[i];
+    }
+
+    // Calculate turnaround times and total turnaround time
+    for (int i = 0; i < n; i++) {
+        turnaround_time[i] = burst_time[i] + wait_time[i];
+        total_turnaround_time += turnaround_time[i];
+    }
+
+    // Calculate and display the average waiting and turnaround times
+    float avg_wait_time = (float)total_wait_time / n;
+    float avg_turnaround_time = (float)total_turnaround_time / n;
+
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\n", i + 1, burst_time[i], wait_time[i], turnaround_time[i]);
+    }
+
+    printf("Average Waiting Time = %.2f\n", avg_wait_time);
+    printf("Average Turnaround Time = %.2f\n", avg_turnaround_time);
+
+    return 0;
+}
+
